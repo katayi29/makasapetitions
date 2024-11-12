dc950b8d0fbeff0e58f73f5e7c53e7cc7bceb66b
katayi29 <katayimakasa29@gmail.com>
2024-11-12 10:09:50 +0100

    project template

---
+++ src/main/java/com/makasa/makasapetitions/MakasapetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.makasa.makasapetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakasapetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakasapetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makasa/makasapetitions/MakasapetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makasa.makasapetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakasapetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
